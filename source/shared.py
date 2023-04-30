912e2af9a2c89e003da4160229fa915cdb7c578a
Frankily <frank.li.fl463@yale.edu>
2023-04-30 16:59:55 -0400

    work in progress

---
+++ source/shared.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+import torchquantum as tq
+
+class SharedWeights(nn.Module):
+    def __init__(self, weight):
+        super().__init__()
+        self.weight = nn.Parameter(weight)
+
+    def forward(self, input, gate_type, wire1, wire2):
+        if gate_type == "CRX":
+            gate = tq.CRX(self.weight, trainable = True)
+        elif gate_type == "CRY":
+            gate = tq.CRY(self.weight, trainable=True)
+        elif gate_type == "CRZ":
+            gate = tq.CRZ(self.weight, trainable=True)
+        else:
+            raise ValueError("Invalid gate type")
+
+        output = gate(input, wire1, wire2)
+        return output
