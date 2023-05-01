ef2e9ba3c23669169ead9df0698ee6cd35212dd5
khnikhil <74874354+khnikhil@users.noreply.github.com>
2023-05-01 12:43:13 -0400

    moving majorana state prep code to state_prep.py

--- source/qcnn.py
+++ source/qcnn.py
@@ -10,6 +10,7 @@ import torch.nn.functional as F
 import torchquantum as tq
 import torchquantum.functional as tqf
 import torchquantum.measurement as tqm
+from state_prep import prepare_majorana, topological_classifier
 
 class QCNN(nn.Module):
     def __init__(self, n_qubits = 8, n_cycles = 4):
@@ -107,26 +108,8 @@ class QCNN(nn.Module):
         # create a quantum device to run the gates
         qdev = tq.QuantumDevice(n_wires=self.n_qubits, device = 'cpu')
 
-        # encode the circuit from the parameters
-        for qub in range(self.n_qubits):
-            tqf.hadamard(qdev,wires= qub)
-
-        for _ in range(self.n_cycles):
-            # for qubit in range(self.n_qubits): #DO Z GATE ON EVERY QUBIT
-            for qub in range(self.n_qubits):
-                tqf.rz(qdev, wires = qub, params = 2*phi)
-            for num in range(self.n_qubits): #DO A XX GATE FOR EVERY NEIGHBORING PAIR OF QUBITS
-                if num%2 ==1:
-                    if num-1 < 0:
-                        pass
-                    else:
-                        tqf.rxx(qdev, wires =[num-1,num], params = 2*theta)
-            for num in range(self.n_qubits): 
-                if num%2 ==0:
-                    if num-1 < 0:
-                        pass
-                    else:
-                        tqf.rxx(qdev, wires =[num-1,num], params = 2*theta)
+        # prepare majorana circuit
+        qdev = prepare_majorana(qdev, self.n_qubits, self.n_cycles, theta, phi)
 
         # STEP 1: add trainable gates for QCNN circuit
         
@@ -274,27 +257,7 @@ class QCNN(nn.Module):
                         shuffle=False, num_workers=0)
 
         return train_loader, train_labels, test_loader, test_labels
-    
-    @staticmethod
-    def topological_classifier(theta, phi):
-        """Given some theta and phi, classifies whether a state is in topological or trivial regime.
-            Args:
-            theta (float): angle of two-qubit RXX gates
-            phi (float): angle of single-qubit RZ gates
-            Returns:
-            label (int): 1 if state is in topological (MZM, MPM, MZM+MPM) regime; 
-                         0 if state is in trivial regime 
-            """
-        if theta <= np.pi/4:
-            if phi <= np.pi/4 and theta <= phi:
-                label = 0
-            elif np.pi/4 < phi and theta < (np.pi/2-phi):
-                label = 0
-            else:
-                label = 1
-        else:
-            label = 1
-        return label
+
     
     @staticmethod
     def get_accuracy(preds, labels, cutoff = 0.2):
